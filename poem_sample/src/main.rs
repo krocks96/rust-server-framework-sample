f8a10f65898127cc35a598099079b183fedac09d
krocks96 <norinorikuronori@gmail.com>
2024-01-11 18:27:30 +0900

    Add poem sample

--- poem_sample/src/main.rs
+++ poem_sample/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{get, handler, listener::TcpListener, web::Path, web::Json, IntoResponse, Route, Server};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Message {
+    message: String,
+}
+
+#[handler]
+fn hello() -> Json<Message> {
+    Json(Message {
+        message: "poem: Hello, world!".to_string(),
+    })
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new().at("/hello", get(hello));
+    Server::new(TcpListener::bind("0.0.0.0:8084"))
+        .run(app)
+        .await
 }
